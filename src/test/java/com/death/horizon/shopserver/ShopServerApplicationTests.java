2acdd53cef63658fe064bdb1ee65db9e35aea12f
DDF-ZTT <a1157790064@gmail.com>
2019-12-28 15:30:56 +0800

    redis and mq 接入

--- src/test/java/com/death/horizon/shopserver/ShopServerApplicationTests.java
+++ src/test/java/com/death/horizon/shopserver/ShopServerApplicationTests.java
@@ -1,16 +1,40 @@
 package com.death.horizon.shopserver;
 
+import com.death.horizon.shopserver.mq.Producer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ShopServerApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+        @Autowired
+        private RedisTemplate redisTemplate;
 
+        @Autowired
+        Producer producer;
+
+        @Test
+        public void contextLoads() {
+                producer.produce();
+        }
+
+        @Test
+        public void testRedis() {
+                redisTemplate.opsForValue().set("testz_key", 123, 60, TimeUnit.SECONDS);
+        }
+
+        @Test
+        public void getRedis() {
+                Object x = redisTemplate.opsForValue().get("testz_key");
+                System.out.println(x);
+        }
 }
